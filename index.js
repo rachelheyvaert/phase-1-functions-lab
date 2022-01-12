812f4a8fdeb19dcd08b2da0268b1967751d62a80
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-11 19:04:44 -0500

    function 1 finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+//Function 1
+let hQ = 42
+function distanceFromHqInBlocks(location) {
+    if (location < 42) {
+    return hQ - location;
+} else { (location >= 42)
+    return location - hQ
+}
+}
+distanceFromHqInBlocks(43);
+
+//function 2
+
