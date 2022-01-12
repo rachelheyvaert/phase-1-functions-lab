4aeb551b513b68fc9c8af4b44e17a09f91694824
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-12 09:24:44 -0500

    completed assignment

--- index.js
+++ index.js
@@ -33,16 +33,14 @@ console.log(destination)
   
 
 //function 4
-let distance = distanceTravelledInFeet(start, destination);
-
-function calculatesFarePrice(distance)
- {
-    console.log("This is the first argument")
+function calculatesFarePrice(start, destination){ 
+    console.log(start, destination)
+    let distance = distanceTravelledInFeet(start, destination);
     if ( distance <= 400) {
         return 0 }
-    else if (distance >= 400 && distance < 2000) {
+    else if (distance > 400 && distance < 2000) {
         return ((distance - 400) * .02)
-    }else if (distance >= 2000 && distance <= 2500) {
+    }else if (distance >= 2001 && distance <= 2500) {
         return 25
     }else { return 'cannot travel that far'}
 }
